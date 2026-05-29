94330e31fb624b0e560d9b4bcccb2526538a7880
Kevin DÃübois <228907418+kevindbois@users.noreply.github.com>
2026-05-29 15:40:40 +0200

    fixed env vars for admin

--- src/main/java/com/webshop/auth_server/config/DataInitializer.java
+++ src/main/java/com/webshop/auth_server/config/DataInitializer.java
@@ -4,9 +4,11 @@ import com.webshop.auth_server.entity.Role;
 import com.webshop.auth_server.entity.User;
 import com.webshop.auth_server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,15 +18,23 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+
     @Override
     public void run(String... args) {
-        if (userRepository.findByUsername("admin@iths.se").isEmpty()) {
+        if (userRepository.findByUsername(adminUsername).isEmpty()) {
             User admin = new User();
-            admin.setUsername("admin@iths.se");
-            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setUsername(adminUsername);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
             admin.setRoles(new HashSet<>(Set.of(Role.ADMIN)));
             userRepository.save(admin);
         }
