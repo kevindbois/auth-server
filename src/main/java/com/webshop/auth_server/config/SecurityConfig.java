09e2f05eab77fdd97f3a351fc80af7493ecb7a83
Kevin DÃübois <228907418+kevindbois@users.noreply.github.com>
2026-06-09 13:44:41 +0200

    added swagger + fixes

--- src/main/java/com/webshop/auth_server/config/SecurityConfig.java
+++ src/main/java/com/webshop/auth_server/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login", "/auth/jwks").permitAll()
+                        .requestMatchers("/auth/register", "/auth/login", "/auth/jwks", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
